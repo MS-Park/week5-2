74c4e9e0ffeee7db14165a7317e0a2b130ca3edb
MS-Park <deceglie92@gmail.com>
2016-04-15 04:36:57 +0900

    first

---
+++ src/main/java/co/test/week5_2/Plan.java
@@ -0,0 +1,8 @@
+package co.test.week5_2;
+
+public interface Plan {
+	public double getBasicCost();
+	public double getOverchargeCost();
+	public double getAdditionalLineCost();
+	public double getFamilyDiscountcost();
+}
